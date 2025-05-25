67046deef38fce446989944d68866cf1d5e9de5a
JuanPabloOviedo <oviedo.herrera.07@gmail.com>
2025-05-25 18:15:46 -0500

    Vistas

--- app/src/main/java/com/example/project_1/MainActivity11.kt
+++ app/src/main/java/com/example/project_1/MainActivity11.kt
@@ -4,29 +4,38 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.project_1.ui.theme.Project_1Theme
+import com.example.project_1.view.Lego
+import com.example.project_1.view.Scroll
 
+/*
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             Project_1Theme {
+                Scroll()
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 
-
-
+                }
             }
         }
     }
 }
 
+
+
+@Preview (showSystemUi = true, showBackground = true)
 @Composable
-fun Horizontal(){
-    val pagerState = rememberPagerState(pageCount = {3})
-    HorizontalPager() { }
-}
+fun GreetingPreview(){
+    Project_1Theme {
+        Lego()
 
-
+    }
+}*/
